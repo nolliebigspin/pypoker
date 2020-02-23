a7725289fb6ffd679475c3dad91e992ca3a257c7
nolliebigspin <alec007@t-online.de>
2020-02-23 14:02:32 +0100

    init

---
+++ card.py
@@ -0,0 +1,6 @@
+class card:
+
+    def __init__(self, color, number):
+        self.color = color
+        self.number = number
+    
---
+++ carddeck.py
@@ -0,0 +1,10 @@
+class carddeck:
+
+    import card.py
+
+    def __init__(self):
+        deck = createDeck()
+
+    def createDeck():
+        x = 3
+        
